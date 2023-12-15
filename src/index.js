01f2375d4993bfa7f705bbb70f04e9aaf76b0ab7
akhtarnasim1990 <akhtar.nasim1990@gmail.com>
2023-12-16 04:01:20 +0530

    basic flight availability application created

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./custom.scss";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
